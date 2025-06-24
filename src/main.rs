8db6d5794ccb0e1547f1f28432c3b5a801400ec6
Sagar Taunk <sagartaunk2@gmail.com>
2025-06-24 06:58:37 +0530

    Added Code

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ use crossterm::{
 };
 use std::{fs::File, io::{self , BufRead , BufReader , Stdout , Write}, os::unix::fs::lchown, process::exit};
 
+enum EditorMode {
+    Normal , 
+    Insert
+}
+
 fn read_file_to_buffer(filename: &str) -> io::Result<Vec<String>> {
     let file = match File::open(filename) {
         Ok(f) => f , 
@@ -18,12 +23,20 @@ fn read_file_to_buffer(filename: &str) -> io::Result<Vec<String>> {
     Ok(reader.lines().filter_map(Result::ok).collect())
 }
 
-fn draw(stdout: &mut Stdout, buffer: &[String], cursor_x: usize, cursor_y: usize) -> std::io::Result<()> {
+fn draw(stdout: &mut Stdout, buffer: &[String], cursor_x: usize, cursor_y: usize , mode :  &EditorMode ) -> std::io::Result<()> {
 
     execute!(stdout , Clear(ClearType::All))?;
     for (i , line)  in buffer.iter().enumerate() {
         execute!(stdout , MoveTo(0 , i as u16))?;
         print!("{}" , line);
+    }
+    execute!(
+        stdout,
+        MoveTo(0, buffer.len() as u16 + 1),
+    )?;
+    match mode {
+        EditorMode::Insert => print!("-- INSERT --"),
+        EditorMode::Normal => print!("-- NORMAL -- (press 'i' to insert, 'q' to quit)"),
     }
     let safe_y = cursor_y.min(buffer.len().saturating_sub(1));
     let safe_x = cursor_x.min(buffer.get(safe_y).map_or(0 , |l| l.len()));
@@ -34,6 +47,7 @@ fn draw(stdout: &mut Stdout, buffer: &[String], cursor_x: usize, cursor_y: usize
 }
 
 fn main() -> std::io::Result<()> {
+    let mut mode = EditorMode::Normal;
     let mut fname = String::new();
     io::stdin().read_line(&mut fname).expect("Failed to read line name");
     let fname = fname.trim();
@@ -46,38 +60,63 @@ fn main() -> std::io::Result<()> {
     let mut stdout = io::stdout();
     execute!(stdout , EnterAlternateScreen , Hide)?;
 
-    loop{
-        draw(&mut stdout , &buffer , cursor_x , cursor_y)?;
+ loop {
+    draw(&mut stdout, &buffer, cursor_x, cursor_y , &mode)?;
 
-        if let Event::Key(KeyEvent {code , .. }) = read()? {
-            match code {
-                KeyCode::Char('Q') => break , 
+    if let Event::Key(KeyEvent { code, .. }) = read()? {
+        match mode {
+            EditorMode::Normal => match code {
+                KeyCode::Char('q') => break,
+                KeyCode::Char('i') => mode = EditorMode::Insert,
                 KeyCode::Up => {
                     if cursor_y > 0 {
-                        cursor_y -= 1 ;
+                        cursor_y -= 1;
                         cursor_x = cursor_x.min(buffer[cursor_y].len());
                     }
-                },
+                }
                 KeyCode::Down => {
-                    if cursor_y + 1 < buffer.len(){
-                        cursor_y += 1 ;
+                    if cursor_y + 1 < buffer.len() {
+                        cursor_y += 1;
                         cursor_x = cursor_x.min(buffer[cursor_y].len());
                     }
-                },
+                }
                 KeyCode::Left => {
                     if cursor_x > 0 {
-                        cursor_x -=1 ;
+                        cursor_x -= 1;
                     }
-                },
+                }
                 KeyCode::Right => {
                     if cursor_x < buffer.get(cursor_y).map_or(0, |l| l.len()) {
                         cursor_x += 1;
                     }
-                },
+                }
+                _ => {}
+            },
+
+            EditorMode::Insert => match code {
+                KeyCode::Esc => mode = EditorMode::Normal,
+                KeyCode::Char(c) => {
+                    if let Some(line) = buffer.get_mut(cursor_y) {
+                        if cursor_x <= line.len() {
+                            line.insert(cursor_x, c);
+                            cursor_x += 1;
+                        }
+                    }
+                }
+                KeyCode::Backspace => {
+                    if let Some(line) = buffer.get_mut(cursor_y) {
+                        if cursor_x > 0 && cursor_x <= line.len() {
+                            line.remove(cursor_x - 1);
+                            cursor_x -= 1;
+                        }
+                    }
+                }
                 _ => {}
             }
         }
     }
+}
+
     disable_raw_mode()?;
     execute!(stdout ,  Show , LeaveAlternateScreen)?;
     Ok(())
